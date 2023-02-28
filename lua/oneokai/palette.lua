4a781e2a1bcd93361273176437a74d7dee018aec
Axel Gard <axel.gard@protonmail.com>
2023-02-28 09:59:17 +0100

    reset colors somthing broke

--- lua/oneokai/palette.lua
+++ lua/oneokai/palette.lua
@@ -16,7 +16,7 @@ local col = {
 
 return {
 
-  dark = {
+	dark = {
 		black = "#181a1f",
 		bg0 = "#282c34",
 		bg1 = "#31353f",
@@ -26,24 +26,26 @@ return {
 		bg_blue = "#73b8f1",
 		bg_yellow = "#ebd09c",
 		fg = "#abb2bf",
-		purple = col.red,
-		green = col.yellow,
+		purple = "#c678dd",
+		green = "#98c379",
 		orange = "#d19a66",
-		blue = col.green,
+		blue = "#61afef",
 		yellow = "#e5c07b",
 		cyan = "#56b6c2",
-		red = col.orange, 
+		red = "#e86671",
 		grey = "#5c6370",
 		light_grey = "#848b98",
 		dark_cyan = "#2b6f77",
-		dark_red = col.orange,
+		dark_red = "#993939",
 		dark_yellow = "#93691d",
-		dark_purple = col.red,
+		dark_purple = "#8a3fa0",
 		diff_add = "#31392b",
 		diff_delete = "#382b2c",
 		diff_change = "#1c3448",
 		diff_text = "#2c5372",
 	},
+	
+
 	cool = {
 		black = "#151820",
 		bg0 = "#242b38",
